3a3492b8ee2daca5bfbee546c23c23c070d80916
Warren MacEvoy <wmacevoy@gmail.com>
2025-09-09 00:42:00 -0600

    cleaner

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,67 +1,211 @@
+#include <tuple>
 #include <array>
-#include <set>
 #include <stdint.h>
-#include "capped_cache.h"
 #include <string>
 #include <iostream>
 
-template <typename T>
-bool lt(const std::set<T> &a, const std::set<T> &b)
+// single threaded choice (1..9) for suduko option tracking
+class choices_t
 {
-  if (a.size() != b.size())
+private:
+  uint8_t _size;
+
+public:
+  uint8_t size() const noexcept { return _size; }
+
+private:
+  // _first = 10 when the set is empty
+  uint8_t _first;
+
+public:
+  uint8_t first() const noexcept { return _first; }
+
+private:
+  uint16_t _bits;
+
+public:
+  uint16_t bits() const noexcept { return _bits; }
+
+public:
+  choices_t() noexcept { all(); }
+
+public:
+  void all() noexcept
   {
-    return a.size() < b.size();
+    _bits = 0b111'1111'1110;
+    _size = 9;
+    _first = 1;
   }
-  auto i = a.begin();
-  auto j = b.begin();
-  while (i != a.end() && j != b.end())
+
+public:
+  void none() noexcept
   {
-    if (*i < *j)
+    _bits = 0b000'0000'0000;
+    _size = 0;
+    _first = 10;
+  }
+
+  void one(uint8_t choice) noexcept
+  {
+    if (1 <= choice && choice <= 9)
     {
+      _bits = 1 << choice;
+      _size = 1;
+      _first = choice;
+    }
+    else
+    {
+      none();
+    }
+  }
+
+  bool is_one(uint8_t choice) const noexcept
+  {
+    return _size == 1 && _first == choice;
+  }
+
+public:
+  bool contains(uint8_t choice) const noexcept { return choice < 10 && ((_bits & (1 << choice)) != 0); }
+
+public:
+  bool insert(uint8_t choice) noexcept
+  {
+    if (choice == 0 || choice > 9)
+      return false;
+    if (!contains(choice))
+    {
+      _bits ^= (1 << choice);
+      ++_size;
+      if (choice < _first)
+        _first = choice;
       return true;
     }
-    if (*j < *i)
+    else
     {
       return false;
     }
-    ++i;
-    ++j;
   }
-  return false;
-}
 
-// 0 is unselected 1-9 is fixed
+public:
+  bool remove(uint8_t choice) noexcept
+  {
+    if (!contains(choice))
+      return false;
+    _bits ^= (1 << choice);
+    --_size;
+    if (choice == _first)
+    {
+      if (_size == 0)
+      {
+        _first = 10;
+      }
+      else
+      {
+        do
+        {
+          ++_first;
+        } while (((1 << _first) & _bits) == 0);
+      }
+    }
+    return true;
+  }
+
+  uint8_t next(uint8_t choice) const noexcept
+  {
+    if (choice < _first)
+      return _first;
+    do
+    {
+      ++choice;
+    } while (choice < 10 && !contains(choice));
+    return choice;
+  }
+
+public:
+  bool operator<(const choices_t &to) const noexcept { return _bits < to._bits; }
+
+public:
+  bool operator<=(const choices_t &to) const noexcept { return _bits <= to._bits; }
+
+public:
+  bool operator==(const choices_t &to) const noexcept { return _bits == to._bits; }
+
+public:
+  bool operator!=(const choices_t &to) const noexcept { return _bits != to._bits; }
+
+public:
+  bool operator>=(const choices_t &to) const noexcept { return _bits >= to._bits; }
+
+public:
+  bool operator>(const choices_t &to) const noexcept { return _bits > to._bits; }
+
+public:
+  int hash() const noexcept { return _bits ^ 0x101'1010'1010; }
+};
+
+template <>
+struct std::hash<choices_t>
+{
+  std::size_t operator()(choices_t const &s) const noexcept { return s.hash(); }
+};
+
+struct counts_t
+{
+  std::array<uint8_t, 9> bins;
+  counts_t() { all(); }
+  void all() { bins.fill(9); }
+  uint8_t remove(uint8_t choice) { return (1 <= choice && choice <= 9 && bins[choice - 1] > 0) ? --bins[choice - 1] : 0; }
+  uint8_t options(uint8_t choice) const { return (1 <= choice && choice <= 9) ? bins[choice - 1] : 0; }
+};
+
 using puzzle_t = std::array<std::array<uint8_t, 9>, 9>;
 
-// game is the set of options at each cell
 struct game
 {
-  using choices_t = std::array<std::array<std::set<uint8_t>, 9>, 9>;
-  choices_t ok;
+  using board_t = std::array<std::array<choices_t, 9>, 9>;
+  board_t board;
+  counts_t rows[9], cols[9], boxes[3][3];
 
-  // reset choices to a given puzzle
-  bool reset(const puzzle_t &puzzle)
+  void all()
   {
-    std::set<uint8_t> all;
-    for (uint8_t choice = 1; choice <= 9; ++choice)
-    {
-      all.insert(choice);
-    }
     for (int i = 0; i < 9; ++i)
     {
       for (int j = 0; j < 9; ++j)
       {
-        ok[i][j] = all;
+        board[i][j].all();
       }
     }
 
     for (int i = 0; i < 9; ++i)
     {
+      rows[i].all();
+    }
+    for (int j = 0; j < 9; ++j)
+    {
+      cols[j].all();
+    }
+    for (int i0 = 0; i0 < 3; ++i0)
+    {
+      for (int j0 = 0; j0 < 3; ++j0)
+      {
+        boxes[i0][j0].all();
+      }
+    }
+  }
+
+  // reset choices to a given puzzle (false means puzzle is impossible, true means it might be solvable)
+  bool reset(const puzzle_t &puzzle)
+  {
+    all();
+
+    for (int i = 0; i < 9; ++i)
+    {
       for (int j = 0; j < 9; ++j)
       {
-        if (puzzle[i][j] > 0)
+        uint8_t choice = puzzle[i][j];
+        if (1 <= choice && choice <= 9)
         {
-          if (!choose(i, j, puzzle[i][j]))
+          if (!choose(i, j, choice))
           {
             return false;
           }
@@ -69,168 +213,179 @@ struct game
       }
     }
     return true;
-  }
-
-  // some cell has no choices
-  bool failed() const
-  {
-    return minimum() == 0;
-  }
-
-  // all cells have 1 choice
-  bool finished() const
-  {
-    return minimum() == 1;
-  }
-
-  bool propegate_isolation(int i, int j, uint8_t choice)
-  {
-    auto &okij = ok[i][j];
-    auto at = okij.find(choice);
-    if (at != okij.end())
-    {
-      okij.erase(at);
-      if (okij.size() == 1)
-      {
-        if (!isolate(i, j, *okij.begin()))
-        {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  // check if choice is hidden because it does
-  // not exist elsewhere in a row / column / or block
-  bool hidden(int i, int j, uint8_t choice) const
-  {
-    bool hid = true;
-    for (int i1 = 0; i1 < 9; ++i1)
-    {
-      if (i1 != i && ok[i1][j].find(choice) != ok[i1][j].end())
-      {
-        hid = false;
-        break;
-      }
-    }
-    if (!hid)
-      return false;
-    for (int j1 = 0; j1 < 9; ++j1)
-    {
-      if (j1 != j && ok[i][j1].find(choice) != ok[i][j1].end())
-      {
-        hid = false;
-        break;
-      }
-    }
-    if (!hid)
-      return false;
-    int i0 = 3 * (i / 3);
-    int j0 = 3 * (j / 3);
-    for (int di = 0; di < 3; ++di)
-    {
-      for (int dj = 0; dj < 3; ++dj)
-      {
-        int i1 = i0 + di, j1 = j0 + dj;
-        if (i != i1 || j != j1)
-          if (ok[i1][j1].find(choice) != ok[i1][j1].end())
-          {
-            hid = false;
-            goto done;
-          }
-      }
-    }
-  done:
-    return hid;
   }
 
   bool choose(int i, int j, uint8_t choice)
   {
-    if (!isolate(i, j, choice))
-      return false;
-    for (int i1 = 0; i1 < 9; ++i1)
-    {
-      for (int j1 = 0; j1 < 9; ++j1)
-      {
-        if (ok[i1][j1].size() == 1)
-          continue;
-        std::set<std::tuple<int, int, uint8_t>> choices;
-        for (auto choice : ok[i1][j1])
-        {
-          if (hidden(i1, j1, choice))
-          {
-            choices.emplace(i1, j1, choice);
-          }
-        }
-        for (auto [i1, j1, choice] : choices)
-        {
-          if (!choose(i1, j1, choice))
-          {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-
-  // remove choices implied by this choice
-  bool
-  isolate(int i, int j, uint8_t choice)
-  {
-    if (ok[i][j].find(choice) == ok[i][j].end())
+    auto &boardij = board[i][j];
+    if (!boardij.contains(choice))
     {
       return false;
     }
-    ok[i][j].clear();
-    ok[i][j].insert(choice);
-
-    // remove choice from row
-    for (int j1 = 0; j1 < 9; ++j1)
+    if (boardij.size() == 1)
     {
-      if (j1 != j)
-        propegate_isolation(i, j1, choice);
+      return true;
     }
-
-    // remove choice from column
-    for (int i1 = 0; i1 < 9; ++i1)
+    for (uint8_t unchoice = boardij.first(); unchoice != 10; unchoice = boardij.next(unchoice))
     {
-      if (i1 != i)
-        propegate_isolation(i1, j, choice);
+      if (unchoice == choice)
+        continue;
+      if (!unchoose(i, j, unchoice))
+        return false;
     }
-
-    // remove choice from subsquare
-    int i0 = 3 * (i / 3);
-    int j0 = 3 * (j / 3);
-    for (int di = 0; di < 3; ++di)
-    {
-      for (int dj = 0; dj < 3; ++dj)
-      {
-        int i1 = i0 + di, j1 = j0 + dj;
-        if (i != i1 || j != j1)
-          propegate_isolation(i1, j1, choice);
-      }
-    }
-    return true;
+    return boardij.is_one(choice);
   }
 
-  // minimum size of choices (ok.size >= 2)
-  int minimum() const
+  bool unchoose(int i, int j, uint8_t unchoice)
   {
-    int ans = 10;
+    auto &boardij = board[i][j];
+    if (!boardij.contains(unchoice))
+    {
+      return true;
+    }
+    boardij.remove(unchoice);
+
+    uint8_t bi = i / 3, bj = j / 3, i0 = 3 * bi, j0 = 3 * bj;
+    auto &row = rows[i];
+    auto &col = cols[j];
+    auto &box = boxes[bi][bj];
+
+    row.remove(unchoice);
+    col.remove(unchoice);
+    box.remove(unchoice);
+
+    bool propegate = true;
+    bool single = false;
+    bool row_chosen = false;
+    bool col_chosen = false;
+    bool box_chosen = false;
+
+    while (propegate)
+    {
+      propegate = false;
+
+      if (!single && boardij.size() == 1)
+      {
+        single = true;
+
+        uint8_t choice = boardij.first();
+        if (row.options(choice) > 1)
+          for (int j1 = 0; j1 < 9; ++j1)
+          {
+            if (j1 == j)
+              continue;
+            if (board[i][j1].contains(choice))
+            {
+              propegate = true;
+              if (!unchoose(i, j1, choice))
+                return false;
+            }
+          }
+        if (col.options(choice) > 1)
+          for (int i1 = 0; i1 < 9; ++i1)
+          {
+            if (i1 == i)
+              continue;
+            if (board[i1][j].contains(choice))
+            {
+              propegate = true;
+              if (!unchoose(i1, j, choice))
+                return false;
+            }
+          }
+        if (box.options(choice) > 1)
+          for (int di = 0; di < 3; ++di)
+          {
+            for (int dj = 0; dj < 3; ++dj)
+            {
+              int i1 = i0 + di, j1 = j0 + dj;
+              if (i1 == i && j1 == j)
+                continue;
+              if (board[i1][j1].contains(choice))
+              {
+                propegate = true;
+                if (!unchoose(i1, j1, choice))
+                  return false;
+              }
+            }
+          }
+      }
+
+      if (!row_chosen && row.options(unchoice) == 1)
+      {
+        for (int j1 = 0; j1 < 9; ++j1)
+        {
+          if (board[i][j1].size() > 1 && board[i][j1].contains(unchoice))
+          {
+            propegate = true;
+            if (!choose(i, j1, unchoice))
+              return false;
+            row_chosen = true;
+            break;
+          }
+        }
+      }
+
+      if (!col_chosen && col.options(unchoice) == 1)
+      {
+        for (int i1 = 0; i1 < 9; ++i1)
+        {
+          if (board[i1][j].size() > 1 && board[i1][j].contains(unchoice))
+          {
+            propegate = true;
+            if (!choose(i1, j, unchoice))
+              return false;
+            col_chosen = true;
+            break;
+          }
+        }
+      }
+
+      if (!box_chosen && box.options(unchoice) == 1)
+      {
+        for (int di = 0; di < 3; ++di)
+        {
+          for (int dj = 0; dj < 3; ++dj)
+          {
+            int i1 = i0 + di, j1 = j0 + dj;
+            if (board[i1][j1].size() > 1 && board[i1][j1].contains(unchoice))
+            {
+              propegate = true;
+              if (!choose(i1, j1, unchoice))
+                return false;
+              box_chosen = true;
+              goto done;
+            }
+          }
+        }
+      done:;
+      }
+    }
+    return !boardij.contains(unchoice);
+  }
+
+  std::pair<int, int> minimum() const
+  {
+    std::pair<int, int> ans(-1, -1);
+    int min = 10;
     for (int i = 0; i < 9; ++i)
     {
       for (int j = 0; j < 9; ++j)
       {
-        int size = ok[i][j].size();
-        if (size != 1 && size < ans)
+        int size = board[i][j].size();
+        if (size != 1 && size < min)
         {
-          ans = size;
+          min = size;
+          ans = std::pair<int, int>(i, j);
         }
       }
     }
-    return ans < 10 ? ans : 1;
+    if (min == 10)
+    {
+      // all sizes were 1 - no choices to make
+      ans = std::pair<int, int>(0, 0);
+    }
+    return ans;
   }
 
   bool operator<(const game &to) const
@@ -239,11 +394,11 @@ struct game
     {
       for (int j = 0; j < 9; ++j)
       {
-        if (lt(ok[i][j], to.ok[i][j]))
+        if (board[i][j] < to.board[i][j])
         {
           return true;
         }
-        if (lt(to.ok[i][j], ok[i][j]))
+        if (to.board[i][j] < board[i][j])
         {
           return false;
         }
@@ -251,83 +406,45 @@ struct game
     }
     return false;
   }
-
-  int chosen() const
-  {
-    int ans = 0;
-    for (int i = 0; i < 9; ++i)
-    {
-      for (int j = 0; j < 9; ++j)
-      {
-        if (ok[i][j].size() == 1)
-          ++ans;
-      }
-    }
-    return ans;
-  }
 };
-
-capped_cache::capped_lru_treeset<game, 100'000> failed;
 
 bool solve(game &state)
 {
-  if (failed.contains(state))
-  {
-    return false;
-  }
-  int min = state.minimum();
-  if (min == 0)
-  {
-    return false;
-  }
-  if (min == 1)
-  {
-    return true;
-  }
+  auto [i, j] = state.minimum();
 
-  for (int i = 0; i < 9; ++i)
+  const auto &boardij = state.board[i][j];
+  if (boardij.size() < 2)
   {
-    for (int j = 0; j < 9; ++j)
+    return boardij.size() == 1;
+  }
+  for (auto choice = boardij.first(); choice != 10; choice = boardij.next(choice))
+  {
+    game substate(state);
+    if (substate.choose(i, j, choice) && solve(substate))
     {
-      const auto &okij = state.ok[i][j];
-      if (okij.size() != min)
-        continue;
-      for (auto choice : okij)
-      {
-        game substate(state);
-        substate.choose(i, j, choice);
-        if (solve(substate))
-        {
-          state = substate;
-          return true;
-        }
-      }
+      state = substate;
+      return true;
     }
   }
-  failed.insert(state);
   return false;
 }
 
 bool solve(puzzle_t &puzzle)
 {
-  failed.clear();
   game state;
-  state.reset(puzzle);
-  if (solve(state))
+  if (!(state.reset(puzzle) && solve(state)))
   {
-    for (int i = 0; i < 9; ++i)
-    {
-      for (int j = 0; j < 9; ++j)
-      {
-        puzzle[i][j] = *state.ok[i][j].begin();
-      }
-    }
-    return true;
-  }
-  else
-  {
+    std::cout << "unsolvable" << std::endl;
     return false;
   }
+  for (int i = 0; i < 9; ++i)
+  {
+    for (int j = 0; j < 9; ++j)
+    {
+      puzzle[i][j] = state.board[i][j].first();
+    }
+  }
+  return true;
 }
 
 char encode(uint8_t value)
@@ -385,11 +502,6 @@ int main()
   print(puzzle);
   if (solve(puzzle))
   {
-    game check;
-    if (!check.reset(puzzle) || !check.finished())
-    {
-      std::cout << "check failed" << std::endl;
-    }
     print(puzzle);
   }
   else
